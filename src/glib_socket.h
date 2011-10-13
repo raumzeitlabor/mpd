9bce739b713b7618371399fc0307d95acebc54f6
Alexander Wirt <formorer@debian.org>
2011-10-13 12:27:18 +0200

    Imported Upstream version 0.16.5

--- src/glib_socket.h
+++ src/glib_socket.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2010 The Music Player Daemon Project
+ * Copyright (C) 2003-2011 The Music Player Daemon Project
  * http://www.musicpd.org
  *
  * This program is free software; you can redistribute it and/or modify
@@ -17,44 +17,24 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef MPD_URI_H
-#define MPD_URI_H
+#ifndef MPD_GLIB_SOCKET_H
+#define MPD_GLIB_SOCKET_H
 
 #include <glib.h>
 
-#include <stdbool.h>
-
 /**
- * Checks whether the specified URI has a scheme in the form
- * "scheme://".
- */
-G_GNUC_PURE
-bool uri_has_scheme(const char *uri);
-
-G_GNUC_PURE
-const char *
-uri_get_suffix(const char *uri);
-
-/**
- * Returns true if this is a safe "local" URI:
- *
- * - non-empty
- * - does not begin or end with a slash
- * - no double slashes
- * - no path component begins with a dot
- */
-G_GNUC_PURE
-bool
-uri_safe_local(const char *uri);
-
-/**
- * Removes HTTP username and password from the URI.  This may be
- * useful for displaying an URI without disclosing secrets.  Returns
- * NULL if nothing needs to be removed, or if the URI is not
- * recognized.
+ * Portable wrapper for g_io_channel_unix_new() or
+ * g_io_channel_win32_new_socket().
  */
 G_GNUC_MALLOC
-char *
-uri_remove_auth(const char *uri);
+static inline GIOChannel *
+g_io_channel_new_socket(int fd)
+{
+#ifdef G_OS_WIN32
+	return g_io_channel_win32_new_socket(fd);
+#else
+	return g_io_channel_unix_new(fd);
+#endif
+}
 
 #endif
